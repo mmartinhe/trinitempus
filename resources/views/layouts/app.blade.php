27f326aede318ad94ee36ee0a4354559e1e2a4df
Mario Martin <mariomartin1917@gmail.com>
2020-12-21 09:30:54 +0100

    icon añadido

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,15 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="Expires" content="0">
+
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>Trinitempus</title>
+    <!--Icono de pestaña -->
+    <link rel = "shortcut icon" href= {{asset('imagenes/logoTriniTiempo.png')}}/>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,14 +2,12 @@
 @section('content')
 
 <div class="align-items-md-center" id = "logo">
-    <img src="{{asset('imagenes/logoTriniTiempo.png')}}" alt = "logotipo trinitempus">
+    <img src="{{asset('imagenes/logoTriniTiempo.png')}}">
 </div>
 
 <div class="text-left" id = "frase">
     <h1>El tiempo es dinero</h1>
     <h3>Regístrate gratis</h3>
-</div>
-<div class="align-content-lg-center" id = "tablaDemandados">
 </div>
 
 @endsection
