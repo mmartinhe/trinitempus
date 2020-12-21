eff803062da09236d2be717894b84bb1e1448664
Mario Martin <mariomartin1917@gmail.com>
2020-12-21 08:47:59 +0100

    footer añadiddo a la plantilla

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,8 +7,10 @@
 
 <div class="text-left" id = "frase">
     <h1>El tiempo es dinero</h1>
+    <h3>Regístrate gratis</h3>
 </div>
-
+<div class="align-content-lg-center" id = "tablaDemandados">
+</div>
 
 @endsection
 
